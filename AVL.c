100f55cf704fb826cdf54b62f228a5385697bc90
João Torezan <jabtorezan@gmail.com>
2024-12-03 17:32:33 -0300

    fodeu

--- AVL.c
+++ AVL.c
@@ -3,8 +3,22 @@
 //arthur henrique  jardin da cunha pinto
 //joão alberto torezan
 
+
+//entrada 1 int por linha
+//para cada valor int positivo, inserir na árvore AVL
+    //cada rotação deve imprimir: 
+        //RSD[valor dos nós P] "rot simples a direita"
+        //RSE[valor do nó P]   "rot simples a esquerda"
+        //RDD[valor do nó P]   "rot dupla a direita"
+        //RDE[valor do nó P]   "rot dupla a esquerda"
+//valor = 0, imprimir o balanço (-1, 0 ou 1) da raiz, excluir a árvore e iniciar nova leitura
+//valor = -1, imprimir fim e finalizar o programa
+
+//usar scranf e printf
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct arv{
     int valor;
@@ -20,23 +34,12 @@ Arv* criaFolha(int info) {
    return folha;
 }
 
-Arv *inserirArv(Arv* arvore, int valor) {
-    if(arvore == NULL) {
-        return criaFolha(valor);
-    }else if(arvore->valor < valor) {
-        arvore->dir = inserirArv(arvore->dir, valor);
-    }else {
-        arvore->esq = inserirArv(arvore->esq, valor);
-    }
-    return arvore;
-}
-
 int altura(Arv* arvore){
     if(arvore == NULL){
-        return -1;
+        return 0;
     }else{
-        int direita = altura(arvore->dir),
-        esquerda = altura(arvore->esq);
+        int direita = altura(arvore->dir);
+        int esquerda = altura(arvore->esq);
 
         if(direita > esquerda){
             return direita + 1;
@@ -45,6 +48,32 @@ int altura(Arv* arvore){
         }
     } 
 }
+
+Arv *inserirArv(Arv* arvore, int valor) {
+    if(arvore == NULL) {
+        return criaFolha(valor);
+    }else if(arvore->valor < valor) {
+        arvore->dir = inserirArv(arvore->dir, valor);
+    }else {
+        arvore->esq = inserirArv(arvore->esq, valor);
+    }
+
+    int direita = altura(arvore->dir),
+    esquerda = altura(arvore->esq);
+
+    printf("%d, d: %d, e: %d\n",arvore->valor, direita, esquerda);
+    
+    if((direita - esquerda) >= 2) {
+        printf("direita\n");
+    }
+     if((esquerda - direita) >= 2) {
+        printf("esquerda\n");
+    }
+    printf("\n");
+    return arvore;
+}
+
+
 
 void printArv(Arv* arvore, int space) {
     if (arvore == NULL) {
@@ -67,7 +96,7 @@ int main() {
 
     Arv * pinheiro = NULL;
 
-    pinheiro = inserirArv(pinheiro, 147);
+    pinheiro = inserirArv(pinheiro, 14);
     pinheiro = inserirArv(pinheiro, 7);
     pinheiro = inserirArv(pinheiro, 10);
     pinheiro = inserirArv(pinheiro, 15);
@@ -79,6 +108,29 @@ int main() {
     printf("\n");
 
     printArv(pinheiro, 0);
+    printf("\n");
+
+
+
+/* 
+Arv * pinheiro = NULL;
+int valor = 0;
+
+while(){
+    scanf("%d", &valor);
+
+    if(valor == 0){
+
+        // imprimir o balanço (-1, 0 ou 1) da raiz, excluir a árvore e iniciar nova leitura
+
+    }else if(valor == -1){
+        printf("\n FIM");
+        break;
+    }else{
+        pinheiro = inserirArv(pinheiro, valor);
+    }
+}
+*/
 
     return 0;
 }
